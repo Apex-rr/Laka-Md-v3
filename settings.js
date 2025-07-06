8319b30ab254503fd539a6023f4377eeca7c05ce
Apex-rr <Sandarup255@gmail.com>
2025-07-06 15:41:13 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,6 +4,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Session id",
+SESSION_ID: process.env.SESSION_ID || "𝐋ᴀᴋᴀ-𝐌ᴅ=g4ARmBZQ#e_WzF3Z5-fjHu4Xc5_5bInuRGgZrIbLSYN7BK6uFkUs",
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
